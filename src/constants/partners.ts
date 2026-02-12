e84f25e769e9d2854f1383345d4729dc5f21d7e9
Lemagiciendelombre <booba8033@gmail.com>
2026-02-12 08:51:26 +0000

    Mise à jour des chemins des images

--- src/constants/partners.ts
+++ src/constants/partners.ts
@@ -3,26 +3,26 @@ import type { Partner } from "@/types";
 export const PARTNERS: Partner[] = [
   {
     name: "MTN Mobile Money",
-    logo: "src/assets/images/slide/mtn.png",
+    logo: "/images/slide/mtn.png",
   },
   {
     name: "Orange Money",
-    logo: "src/assets/images/slide/orange_money.jpg",
+    logo: "/images/slide/orange_money.jpg",
   },
   {
     name: "LAGUIPRES Sarl",
-    logo: "src/assets/images/slide/lagui.jpeg",
+    logo: "/images/slide/lagui.jpeg",
   },
   {
     name: "PricewhiterhouseCoopers",
-    logo: "src/assets/images/slide/pwc.png",
+    logo: "/images/slide/pwc.png",
   },
   {
     name: "BATIPRO",
-    logo: "src/assets/images/slide/batipro.png",
+    logo: "/images/slide/batipro.png",
   },
   {
     name: "Performance-Guinée",
-    logo: "src/assets/images/slide/performance.jpeg",
+    logo: "/images/slide/performance.jpeg",
   },
 ];
