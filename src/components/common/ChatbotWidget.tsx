5b9720c4beb97af1263eb9956baca24ed427f158
Lemagiciendelombre <booba8033@gmail.com>
2026-02-12 12:36:22 +0000

    Resolution problème group_name

--- src/components/common/ChatbotWidget.tsx
+++ src/components/common/ChatbotWidget.tsx
@@ -46,8 +46,6 @@ export function Chatbot({ ws, roomName }: ChatbotProps) {
       }
 
       if (data.type === "chat.message") {
-        const group = data.groupe_name || data.group_name;
-        if (!group) return;
 
         if (data.user_type === "customer") return;
         
