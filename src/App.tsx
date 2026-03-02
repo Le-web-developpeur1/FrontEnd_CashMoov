e59d9b2e1fc88335b964fac3816de62ce9f5a600
Lemagiciendelombre <booba8033@gmail.com>
2026-03-02 09:35:53 +0000

    MJ App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,6 @@ import AssistantConversations from './pages/assistant/AssistantConversations'
 import AssistantChat from './pages/assistant/AssistantChat'
 import ProfilePage from './pages/ProfilePage'
 import AboutPage from './pages/AboutPage'
-import Reseau from './pages/test/Reseau'
 
 function App() {
   return (
@@ -41,7 +40,6 @@ function App() {
             <Route path='/contact' element={<ContactPage />} />
             <Route path='/notre-reseau' element={<NotreReseau />} />
             <Route path='/about' element={<AboutPage />} />
-            <Route path='/reseau' element={<Reseau />} />
             <Route path='*' element={<NotFound />} />
           </Route>
 
